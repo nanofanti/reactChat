438857c4dd54edf3e5b9cbdea02d537a8f2e3048
Marco Fanti <fanti.marco8@gmail.com>
2024-06-24 10:59:53 +0200

    chats and addUser

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,19 +1,42 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
 import styles from "./AddUser.module.css";
+import { db } from "../library/firebase";
+import { useState } from "react";
 
 export default function AddUser() {
+  const [user, setUser] = useState(null);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className={styles.addUser}>
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" id="" />
         <button>Search</button>
       </form>
-      <div className={styles.user}>
-        <div className={styles.detail}>
-          <img src="avatar.png" alt="" />
-          <span>Jane Doe</span>
+      {user && (
+        <div className={styles.user}>
+          <div className={styles.detail}>
+            <img src={user.avatar || "/avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
         </div>
-        <button>Add User</button>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ChatList.module.css";
 import AddUser from "./AddUser";
+import { useUserStore } from "../library/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../library/firebase";
 
 export default function ChatList() {
   const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+          const user = userDocSnap.data();
+          return { ...item, user };
+        });
+        const chatData = await Promise.all(promises);
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
   return (
     <div className={styles.chatlist}>
@@ -16,37 +43,19 @@ export default function ChatList() {
           src={addMode ? "minus.png" : "plus.png"}
           alt=""
           className={styles.plus_image}
-          onClick={() => setAddMode(!addMode)}
+          onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
-      <div className={styles.item}>
-        <img src="avatar.png" alt="" />
-        <div className={styles.texts}>
-          <span>Jane Doe</span>
-          <p>Hello</p>
+      {chats.map((chat) => (
+        <div className={styles.item} key={chat.chatId}>
+          <img src="avatar.png" alt="" />
+          <div className={styles.texts}>
+            <span>Jane Doe</span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.item}>
-        <img src="avatar.png" alt="" />
-        <div className={styles.texts}>
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <img src="avatar.png" alt="" />
-        <div className={styles.texts}>
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <img src="avatar.png" alt="" />
-        <div className={styles.texts}>
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
+
       {addMode && <AddUser />}
     </div>
   );
